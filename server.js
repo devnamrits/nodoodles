bd6818b531473453a07e81747ec54e308d1479ae
Hritik Kumar <hrk10321@gmail.com>
2021-08-14 20:53:46 +0530

    Basic routing and headers

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    res.end("Hello everyone");
+    const pathName = req.url;
+    console.log(pathName);
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is OVERVIEW page');
+    }
+    else if(pathName === '/product'){
+        res.end('This is PRODUCT page');
+    }
+    else{
+        res.writeHead(404,{
+            'content-type':'text/html',
+            'my-header':'nothing'
+        })
+        res.end('Page not Found!');
+    }
 })
 
 server.listen(8000, '127.0.0.1', () => {
