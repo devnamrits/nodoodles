fca23ea76d387a9507b8ddfa581c6a55279d5ba8
Hritik Kumar <hrk10321@gmail.com>
2021-08-14 17:12:56 +0530

    Asynchronous Non-Blocking I/O

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 
+// Blocking Synchronous I/O
 const readIn = fs.readFileSync('./txt/input.txt','utf-8');
 
 const readOut = `Hello The input file contains ${readIn}`;
 
 fs.writeFileSync('./txt/output.txt',readOut)
+
+
+// Non-Blocking Asynchronous I/O
+const readInput = fs.readFile('./txt/input.txt', 'utf-8', (err,data) => {
+    console.log(`Read ${data}`);
+})
+console.log('Reading File');
